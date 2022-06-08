fe8b19990c4deec54ddf08a4e7f7601c0b903d6d
Shoyayeb <sa.fine@outlook.com>
2022-06-08 18:20:09 +0600

    login route added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient } = require("mongodb");
-
+const bcrypt = require("bcryptjs");
 require("dotenv").config();
 
 const app = express();
@@ -15,9 +15,6 @@ const client = new MongoClient(process.env.URI, {
     useUnifiedTopology: true,
 });
 
-
-
-
 async function run() {
     try {
         await client.connect();
@@ -27,8 +24,8 @@ async function run() {
         // CREATING A USER WITH -USERNAME, PASSWORD, EMAIIL
         app.post('/register', async (req, res) => {
             const { email, password, username } = req.body;
-
             if (!email || !password || !username) res.send({ status: 400, message: "Incomplete Request" });
+
             else if (email && password && username) {
                 try {
                     const hashedPassword = await bcrypt.hash(password, 10);
@@ -37,9 +34,29 @@ async function run() {
                     res.json({ status: 'ok', user, result });
                 } catch (err) {
                     console.log(err);
-                    res.json({ status: 'error', message: 'Duplicate email' });
+                    res.json({ status: 400, message: err || "Error in Database" });
                 }
             } else res.json({ status: 400, message: "Unknown error" });
+        });
+
+        app.post('/login', async (req, res) => {
+            const { password, username } = req.body;
+            if (!password || !username) {
+                res.send({ status: 400, message: "Incomplete Request" })
+            } else if (password && username) {
+                const user = await usersCollection.findOne({ username });
+                if (!user) {
+                    return { status: 404, error: 'No user found with this email' }
+                } else if (user) {
+                    const isPasswordValid = await bcrypt.compare(password, user.hashedPassword);
+                    if (isPasswordValid) {
+                        res.json({ status: 'ok', user })
+                    } else {
+                        res.json({ status: 401, message: 'Wrong Password' })
+                    }
+                }
+            }
+
         })
 
     } finally {
@@ -49,11 +66,9 @@ async function run() {
 
 run().catch(console.dir);
 
-
-app.get('/', async (req, res) => {
+app.get('/', (req, res) => {
     res.json({ status: 400, message: "server running", uri: process.env.URI })
 });
-
 
 app.listen(port, () => {
     console.log(`Running server on http://localhost:${port}`);
