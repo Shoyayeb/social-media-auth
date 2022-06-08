6ea7acc5bd91a29095d741c8efa28b1b7d8f7af1
Shoyayeb <sa.fine@outlook.com>
2022-06-08 18:50:42 +0600

    removed unused code and fixed login

--- index.js
+++ index.js
@@ -15,56 +15,40 @@ const client = new MongoClient(process.env.URI, {
     useUnifiedTopology: true,
 });
 
-async function run() {
+client.connect().then(async () => {
+    console.log('connected');
+});
+const database = client.db(process.env.DBNAME);
+const usersCollection = database.collection("users");
+
+// CREATING A USER WITH -USERNAME, PASSWORD, EMAIIL
+app.post('/register', async (req, res) => {
+    const { email, password, username } = req.body;
+    if (!email || !password || !username) res.send({ status: 400, message: "Incomplete Request" });
     try {
-        await client.connect();
-        const database = client.db(process.env.DBNAME);
-        const usersCollection = database.collection("users");
-
-        // CREATING A USER WITH -USERNAME, PASSWORD, EMAIIL
-        app.post('/register', async (req, res) => {
-            const { email, password, username } = req.body;
-            if (!email || !password || !username) res.send({ status: 400, message: "Incomplete Request" });
-
-            else if (email && password && username) {
-                try {
-                    const hashedPassword = await bcrypt.hash(password, 10);
-                    const user = { email, hashedPassword, username };
-                    const result = await usersCollection.insertOne(user);
-                    res.json({ status: 'ok', user, result });
-                } catch (err) {
-                    console.log(err);
-                    res.json({ status: 400, message: err || "Error in Database" });
-                }
-            } else res.json({ status: 400, message: "Unknown error" });
-        });
-
-        app.post('/login', async (req, res) => {
-            const { password, username } = req.body;
-            if (!password || !username) {
-                res.send({ status: 400, message: "Incomplete Request" })
-            } else if (password && username) {
-                const user = await usersCollection.findOne({ username });
-                if (!user) {
-                    return { status: 404, error: 'No user found with this email' }
-                } else if (user) {
-                    const isPasswordValid = await bcrypt.compare(password, user.hashedPassword);
-                    if (isPasswordValid) {
-                        res.json({ status: 'ok', user })
-                    } else {
-                        res.json({ status: 401, message: 'Wrong Password' })
-                    }
-                }
-            }
-
-        })
-
-    } finally {
-        // await client.close();
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = { email, hashedPassword, username };
+        const result = await usersCollection.insertOne(user);
+        res.json({ status: 201, message: "User created", result });
+    } catch (err) {
+        console.log(err);
+        res.json({ status: 400, message: err || "Error in Database" });
     }
-}
+});
 
-run().catch(console.dir);
+app.post('/login', async (req, res) => {
+    const { password, username } = req.body;
+    if (!password || !username) {
+        res.send({ status: 400, message: "Incomplete Request" })
+    }
+    await usersCollection.findOne({ username: username }).then(async (result) => {
+        const isPasswordValid = await bcrypt.compare(password, result.hashedPassword);
+        isPasswordValid ? res.json({ status: 200, result }) : res.json({ status: 401, message: 'Wrong Password' });
+    }).catch((err) => {
+        console.log(err, 'error got');
+        res.json({ status: 404, message: 'No user found with this username' });
+    });
+});
 
 app.get('/', (req, res) => {
     res.json({ status: 400, message: "server running", uri: process.env.URI })
